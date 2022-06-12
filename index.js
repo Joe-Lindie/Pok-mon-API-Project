13e261852a43fb1bed53c11a2239b7059437e76c
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-06-12 09:37:43 +0100

    updates to JS containers

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function createElement({
   parent,
   parentSelector,
   text = "",
+  src,
+  alt,
   id,
 }) {
   const parentEl = parent || $(parentSelector)
@@ -29,6 +31,8 @@ function createElement({
   if (className) newElement.classList.add(className)
   if (id) newElement.id = id
   if (parentEl) parentEl.append(newElement)
+  if (src) newElement.src = src
+  if (alt) newElement.alt = alt
 
   return newElement
 }
@@ -40,30 +44,104 @@ form.addEventListener("submit", (event) => {
   const name = pokémonName.value.toLowerCase()
 
   //FETCH API AND CONVERT TO JSON
-  function getPokémons(name) {
-    return fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) =>
-      response.json()
-    )
+  function getPokémons(name, id) {
+    return fetch(
+      `https://pokeapi.co/api/v2/${id ? "characteristics" : "pokemon"}/${
+        id || name
+      }`
+    ).then((response) => response.json())
+
+    // const characteristics = fetch(
+    //   `https://pokeapi.co/api/v2/characteristic/%7Bid%7D/`
+    // ).then((response) => response.json())
+    // return { mainData, characteristics }
   }
 
   //POKÉMON NAME
   getPokémons(name)
     .then((attribute) => {
-      //POKÉMON NAME
-      const name = document.createElement("h2")
-      name.innerHTML = attribute.name
+      console.log(attribute)
+      // //POKÉMON NAME
+      // const name = document.createElement("h2")
+      // name.innerHTML = attribute.name
 
-      poke_name.append(name)
+      // poke_name.append(name)
 
-      //POKÉMON IMAGE
-      const image = document.createElement("img")
-      image.src = attribute.sprites.front_shiny
-      image.alt = "Pokémon Character"
+      // //POKÉMON IMAGE
+      // const image = document.createElement("img")
+      // image.src = attribute.sprites.front_shiny
+      // image.alt = "Pokémon Character"
 
-      poke_img.append(image)
+      // poke_img.append(image)
+      const parentDiv = createElement({
+        tag: "div",
+        className: "card__container",
+        parentSelector: "body",
+      })
+      /* const cardChildren = {
+          pokemonName: 
+  
+}*/
+      createElement({
+        tag: "p",
+        className: "pokemon-name",
+        parent: parentDiv,
+        text: attribute.name,
+      })
+
+      createElement({
+        tag: "p",
+        className: "hp",
+        parent: parentDiv,
+        text: "hp " + attribute.stats[0].base_stat,
+      })
+
+      createElement({
+        tag: "p",
+        className: "element-type",
+        parent: parentDiv,
+        text: attribute.types[0].type.name,
+      })
+
+      createElement({
+        tag: "p",
+        className: "abilities-type",
+        parent: parentDiv,
+        text: attribute.abilities[0].ability.name,
+      })
+
+      createElement({
+        tag: "p",
+        className: "weakness",
+        parent: parentDiv,
+        text: attribute.abilities[0].ability.name,
+      })
+
+      createElement({
+        tag: "p",
+        className: "description",
+        parent: parentDiv,
+        text: attribute.descriptions[0].description,
+      })
+
+      createElement({
+        tag: "img",
+        className: "poke-image",
+        parent: parentDiv,
+        alt: "Pokémon Character",
+        src: attribute.sprites.front_shiny,
+      })
     })
 
     .catch(
       (error) => (pokemonName.innerHTML = "This pokemon does not exist. Sorry!")
     )
 })
+
+/* function createPokemonElements(pokemonObj) {
+  textSections: [{name: 'name'}, hp, type, 
+  img:,
+  
+  
+}
+*/
